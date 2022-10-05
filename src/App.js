3fefc01ab0f80547cb6b62b829ebcb59032f1a34
Spandana1003 <spandanasirisinahal@gmail.com>
2022-10-05 20:35:02 +0530

    authCommit1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
+import { Modal, Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import SignIn from "./Components/SignIn";
+import { useState } from "react";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
+	const [modal, setModal] = useState(false);
+	const toggle = () => setModal(!modal)
 	return (
-		<div className="App">
+		<AuthProvider>
+					<div className="App">
+			<Modal isOpen={modal} toggle={toggle} > <SignIn /> </Modal>
 			<Navbar>
 				<NavbarBrand>Spandu & Manu</NavbarBrand>
 				<Nav>
 					<NavItem className="align-items-center">
-						<NavLink className="text-black">
-							<i class="bi bi-person-fill mx-2"></i>Sign In
+						<NavLink className="text-black" onClick={toggle}>
+							<i className="bi bi-person-fill mx-2"></i>Sign In
 						</NavLink>
 					</NavItem>
 					<NavItem>
 						<NavLink className="text-black">
-							<i class="bi bi-heart-fill mx-1" /> Favorites
+							<i className="bi bi-heart-fill mx-1" /> Favorites
 						</NavLink>
 					</NavItem>
 					<NavItem>
 						<NavLink className="text-black">
-							<i class="bi bi-bag-fill mx-2"></i>Shopping Bag
+							<i className="bi bi-bag-fill mx-2"></i>Shopping Bag
 						</NavLink>
 					</NavItem>
 				</Nav>
 			</Navbar>
+			
 		</div>
+		</AuthProvider>
 	);
 }
 
--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,12 +1,48 @@
-import React, { useState } from 'react'
-import { Button } from 'reactstrap';
+import React, { useRef } from 'react'
+import { Button, Card, CardBody, Form, FormGroup, Input, Label } from 'reactstrap'
+import { useAuth } from '../contexts/AuthContext'
+// import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 export default function SignIn() {
-  const [modal,setModal] = useState(false);
-  const toggle = () => setModal(!modal);
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const {signup} = useAuth()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    signup(emailRef.current.value, passwordRef.current.value)
+  }
   return (
-    <div>
-      <Button onClick={toggle}></Button>
-    </div>
+    <>
+      <Card>
+        <CardBody>
+          <h2 className='text-centre mb-4' >Sign Up</h2>
+          <Form>
+            <FormGroup >
+              <Label for='email'>Email</Label>
+              <Input type= 'email' id='email' 
+              ref={emailRef} required
+              ></Input>
+            </FormGroup>
+            <FormGroup >
+              <Label for='password'>Password</Label>
+              <Input type= 'password' id='password' 
+              ref={passwordRef} required
+              ></Input>
+            </FormGroup>
+            <FormGroup >
+              <Label for='password-confirmation'>Password confirmation</Label>
+              <Input type= 'password' id='password-confirmation' 
+              ref={passwordConfirmRef} required
+              ></Input>
+            </FormGroup>
+            <Button color='primary' className='w-100'>Sign Up</Button>
+          </Form>
+        </CardBody>
+      </Card>
+      <div className='w-100 text-centre mt-2'>Already have an account? Log In</div>
+    </>
   )
 }
