c439e2af3c1c34e010b2d3cabf50638eaefe10dc
ManojKarthikeya <https://github.com/ManojKarthikeya>
2022-10-05 13:53:47 +0530

    added icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar>
-        <NavbarBrand>
-          Spandu & Manu
-        </NavbarBrand>
-        <Nav>
-          <NavItem>
-            <NavLink>Sign In</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>Favorites</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>Shopping Bag</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+				<NavbarBrand>Spandu & Manu</NavbarBrand>
+				<Nav>
+					<NavItem className="align-items-center">
+						<NavLink className="text-black">
+							<i class="bi bi-person-fill mx-2"></i>Sign In
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className="text-black">
+							<i class="bi bi-heart-fill mx-1" /> Favorites
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className="text-black">
+							<i class="bi bi-bag-fill mx-2"></i>Shopping Bag
+						</NavLink>
+					</NavItem>
+				</Nav>
+			</Navbar>
 		</div>
 	);
 }
