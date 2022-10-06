79f32699adb8efb55acbb8ba5760fb2e952cddc3
ManojKarthikeya <https://github.com/ManojKarthikeya>
2022-10-06 18:49:06 +0530

    ah

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -2,67 +2,6 @@ import React from "react";
 import { Card, CardBody, CardFooter, CardHeader, CardLink, CardText, Col, Row } from "reactstrap";
 
 export default function SignIn(props) {
-<<<<<<< HEAD
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const {signUP} = useAuth()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    if(passwordRef.current.value !== passwordConfirmRef.current.value){
-      console.log('hello')
-      return setError('Passwords do not match')
-    }
-
-    try{
-      setError('')
-      setLoading(true)
-      await signUP(emailRef.current.value, passwordRef.current.value)
-    } catch(e){
-      console.log(e)
-      setError('Failed to create an account')
-    }
-
-    props.togglefun()
-    setLoading(false)
-  }
-  return (
-    <>
-      <Card>
-        <CardBody>
-          <h2 className='text-centre mb-4' >Sign Up</h2>
-          {error && <Alert color='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <FormGroup >
-              <Label for='email'>Email</Label>
-              <Input type= 'email' id='email' 
-              innerRef={emailRef} required
-              ></Input>
-            </FormGroup>
-            <FormGroup >
-              <Label for='password'>Password</Label>
-              <Input type= 'password' id='password' 
-              innerRef={passwordRef} required
-              ></Input>
-            </FormGroup>
-            <FormGroup >
-              <Label for='password-confirmation'>Password confirmation</Label>
-              <Input type= 'password' id='password-confirmation' 
-              innerRef={passwordConfirmRef} required
-              ></Input>
-            </FormGroup>
-            <Button disabled={loading} color='primary' className='w-100'>Sign Up</Button>
-          </Form>
-        </CardBody>
-      </Card>
-      <div className='w-100 text-centre mt-2'>Already have an account? Log In</div>
-    </>
-  )
-=======
 	return (
 		<React.Fragment>
 			<Card>
@@ -100,5 +39,4 @@ export default function SignIn(props) {
 			</Card>
 		</React.Fragment>
 	);
->>>>>>> 443d19b567381f9c53f88674abda1bb66021ea82
 }
