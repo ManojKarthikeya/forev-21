72d0064f03b938b52f1b0d3c4bec77f6e6aa43c7
Spandana1003 <spandanasirisinahal@gmail.com>
2022-10-06 18:54:12 +0530

    authcommitidk

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,104 +1,99 @@
-import React from "react";
-import { Card, CardBody, CardFooter, CardHeader, CardLink, CardText, Col, Row } from "reactstrap";
+import React, { useRef, useState } from "react";
+import {
+  Alert,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardLink,
+  CardText,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function SignIn(props) {
-<<<<<<< HEAD
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const {signUP} = useAuth()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { logIn } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(passwordRef.current.value !== passwordConfirmRef.current.value){
-      console.log('hello')
-      return setError('Passwords do not match')
+    try {
+      setError("");
+      setLoading(true);
+      await logIn(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to log in");
     }
 
-    try{
-      setError('')
-      setLoading(true)
-      await signUP(emailRef.current.value, passwordRef.current.value)
-    } catch(e){
-      console.log(e)
-      setError('Failed to create an account')
-    }
-
-    props.togglefun()
-    setLoading(false)
+    props.togglefun();
+    setLoading(false);
   }
   return (
-    <>
+    <React.Fragment>
       <Card>
-        <CardBody>
-          <h2 className='text-centre mb-4' >Sign Up</h2>
-          {error && <Alert color='danger'>{error}</Alert>}
+        <CardHeader>
+          <Row>
+            <Col>
+              <h2 className="display-6 mx-3">Log in</h2>
+            </Col>
+            <Col className="text-sm-end">
+              <h3>
+                <i
+                  onClick={() => {
+                    props.togglefun();
+                  }}
+                  style={{ cursor: "pointer" }}
+                  className="bi bi-x "
+                ></i>
+              </h3>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody className="px-5">
+          {error && <Alert color="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <FormGroup >
-              <Label for='email'>Email</Label>
-              <Input type= 'email' id='email' 
-              innerRef={emailRef} required
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" id="email" innerRef={emailRef}></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                innerRef={passwordRef}
               ></Input>
             </FormGroup>
-            <FormGroup >
-              <Label for='password'>Password</Label>
-              <Input type= 'password' id='password' 
-              innerRef={passwordRef} required
-              ></Input>
-            </FormGroup>
-            <FormGroup >
-              <Label for='password-confirmation'>Password confirmation</Label>
-              <Input type= 'password' id='password-confirmation' 
-              innerRef={passwordConfirmRef} required
-              ></Input>
-            </FormGroup>
-            <Button disabled={loading} color='primary' className='w-100'>Sign Up</Button>
           </Form>
+          <Row className="d-flex justify-content-center">
+            <Button disabled={loading} color="success" className="w-75 my-2">
+              Log In
+            </Button>
+          </Row>
         </CardBody>
+        <CardFooter>
+          <CardText className="mx-3 p-1">
+            Don't have an account?{" "}
+            <CardLink
+              onClick={() => {
+                props.setLog(!props.log);
+              }}
+            >
+              Register
+            </CardLink>
+          </CardText>
+        </CardFooter>
       </Card>
-      <div className='w-100 text-centre mt-2'>Already have an account? Log In</div>
-    </>
-  )
-=======
-	return (
-		<React.Fragment>
-			<Card>
-				<CardHeader>
-					<Row>
-						<Col>
-							<h2 className="display-6 mx-3">Log in</h2>
-						</Col>
-						<Col className="text-sm-end">
-							<h3>
-								<i
-									onClick={() => {
-										props.togglefun();
-									}}
-									style={{ cursor: "pointer" }}
-									className="bi bi-x "
-								></i>
-							</h3>
-						</Col>
-					</Row>
-				</CardHeader>
-				<CardBody className="px-5">hehe</CardBody>
-				<CardFooter>
-					<CardText className="mx-3 p-1">
-						Don't have an account?{" "}
-						<CardLink
-							onClick={() => {
-								props.setLog(!props.log);
-							}}
-						>
-							Register
-						</CardLink>
-					</CardText>
-				</CardFooter>
-			</Card>
-		</React.Fragment>
-	);
->>>>>>> 443d19b567381f9c53f88674abda1bb66021ea82
+    </React.Fragment>
+  );
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,125 +1,123 @@
 import React, { useRef, useState } from "react";
 import {
-	Alert,
-	Button,
-	Card,
-	CardBody,
-	CardFooter,
-	CardHeader,
-	CardLink,
-	CardText,
-	Col,
-	Form,
-	FormGroup,
-	Input,
-	Label,
-	Row,
+  Alert,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardLink,
+  CardText,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
 } from "reactstrap";
 import { useAuth } from "../contexts/AuthContext";
 // import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 export default function SignUp(props) {
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const passwordConfirmRef = useRef();
-	const { signUP } = useAuth();
-	const [error, setError] = useState("");
-	const [loading, setLoading] = useState(false);
-	const [agree, setAgree] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signUP } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  // const [agree, setAgree] = useState(false);
 
-	async function handleSubmit(e) {
-		e.preventDefault();
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-			console.log("hello");
-			return setError("Passwords do not match");
-		}
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      console.log("hello");
+      return setError("Passwords do not match");
+    }
 
-		try {
-			setError("");
-			setLoading(true);
-			await signUP(emailRef.current.value, passwordRef.current.value);
-		} catch (e) {
-			console.log(e);
-			setError("Failed to create an account");
-		}
+    try {
+      setError("");
+      setLoading(true);
+      await signUP(emailRef.current.value, passwordRef.current.value);
+    } catch (e) {
+      // console.log(e);
+      setError("Failed to create an account");
+    }
 
-		props.togglefun();
-		setLoading(false);
-	}
-	return (
-		<React.Fragment>
-			<Card>
-				<CardHeader>
-					<Row>
-						<Col>
-							<h2 className="display-6 mx-3">Register</h2>
-						</Col>
-						<Col className="text-sm-end">
-							<h3>
-								<i
-									onClick={() => {
-										props.togglefun();
-									}}
-									style={{ cursor: "pointer" }}
-									className="bi bi-x "
-								></i>
-							</h3>
-						</Col>
-					</Row>
-				</CardHeader>
-				<CardBody className="px-5">
-					{error && <Alert color="danger">{error}</Alert>}
-					<Form onSubmit={handleSubmit}>
-						<FormGroup>
-							<Label for="email">Email</Label>
-							<Input
-								type="email"
-								id="email"
-								ref={emailRef}
-								required
-							></Input>
-						</FormGroup>
-						<FormGroup>
-							<Label for="password">Password</Label>
-							<Input
-								type="password"
-								id="password"
-								ref={passwordRef}
-								required
-							></Input>
-						</FormGroup>
-						<FormGroup>
-							<Label for="password-confirmation">
-								Password confirmation
-							</Label>
-							<Input
-								type="password"
-								id="password-confirmation"
-								ref={passwordConfirmRef}
-								required
-							></Input>
-						</FormGroup>
-						<Row className="d-flex justify-content-center">
-							<Button
-								disabled={loading}
-								color="success"
-								className=" w-75 my-2"
-							>
-								Register
-							</Button>
-						</Row>
-					</Form>
-				</CardBody>
-				<CardFooter>
-					<CardText className="mx-3 p-1">
-						Already have an account?{" "}
-						<CardLink onClick={() => {
-							props.setLog(!props.log)
-						}}>Log in</CardLink>
-					</CardText>
-				</CardFooter>
-			</Card>
-		</React.Fragment>
-	);
+    props.togglefun();
+    setLoading(false);
+  }
+  return (
+    <React.Fragment>
+      <Card>
+        <CardHeader>
+          <Row>
+            <Col>
+              <h2 className="display-6 mx-3">Register</h2>
+            </Col>
+            <Col className="text-sm-end">
+              <h3>
+                <i
+                  onClick={() => {
+                    props.togglefun();
+                  }}
+                  style={{ cursor: "pointer" }}
+                  className="bi bi-x "
+                ></i>
+              </h3>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody className="px-5">
+          {error && <Alert color="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                innerRef={emailRef}
+                required
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                innerRef={passwordRef}
+                required
+              ></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password-confirmation">Password confirmation</Label>
+              <Input
+                type="password"
+                id="password-confirmation"
+                innerRef={passwordConfirmRef}
+                required
+              ></Input>
+            </FormGroup>
+            <Row className="d-flex justify-content-center">
+              <Button disabled={loading} color="success" className=" w-75 my-2">
+                Register
+              </Button>
+            </Row>
+          </Form>
+        </CardBody>
+        <CardFooter>
+          <CardText className="mx-3 p-1">
+            Already have an account?{" "}
+            <CardLink
+              onClick={() => {
+                props.setLog(!props.log);
+              }}
+            >
+              Log in
+            </CardLink>
+          </CardText>
+        </CardFooter>
+      </Card>
+    </React.Fragment>
+  );
 }
